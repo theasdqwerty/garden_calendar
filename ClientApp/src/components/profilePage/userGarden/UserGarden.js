81ed4aec7442720b45673c7f5a58177c1e0faf75
asdqwerty <theasdqwerty@gmail.com>
2023-11-29 21:43:43 +0300

    saf

--- ClientApp/src/components/profilePage/userGarden/UserGarden.js
+++ ClientApp/src/components/profilePage/userGarden/UserGarden.js
@@ -2,17 +2,57 @@ import React, {useEffect, useState} from 'react';
 import style from './UserGarden.module.css';
 import {Garden} from './garden/Garden';
 
-
-
 export const UserGarden = () => {
     const [lines, setLines] = useState([]);
     const [selectedGardenIndex, setSelectedGardenIndex] = useState(null);
+    
+    const getAllGardens = async () => 
+    {
+        const userId = localStorage.getItem('userId')
+        let response = await fetch(`https://localhost:7135/api/Gardens/GetAllByUserId/${userId}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        
+        const json = await response.json()
+        console.log(json)
+    }
+    
+    const addGarden = async (gardenName) =>
+    {
+        const gardenModel = {
+            Name: gardenName
+        }
+        
+        console.log('accessToken', localStorage.getItem('accessToken'))
 
-    const addLine = () => {
+        let response = await fetch("https://localhost:7135/api/Gardens/Add",
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
+                    
+                },
+                body: JSON.stringify(gardenModel)
+            })
+
+        const json = await response.json()
+        console.log(json)
+    }
+    
+    getAllGardens()
+
+    const addLine = async () => {
         const gardenNumber = lines.length + 1;
         setLines([...lines, <div onClick={() => handleGardenClick(lines.length)} key={lines.length}>
             New Garden{gardenNumber}
         </div>]);
+        
+        // await addGarden(`Огород ${gardenNumber}`)
     };
 
     const handleGardenClick = (index) => {
@@ -31,12 +71,10 @@ export const UserGarden = () => {
                             className={index === selectedGardenIndex ? style.selectedButton : style.button}
                             onClick={() => {
                                 handleGardenClick(index)
-                            }}
-                        >
+                            }}>
                             Сад № {index + 1}
                         </button>
                         {selectedGardenIndex === index && <Garden props={index + 1}/>}
-
                     </div>
                 ))}
             </section>
